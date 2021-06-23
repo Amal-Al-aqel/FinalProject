d8798943aff8e980bb5e526632a17408bf3dbff5
your-name <your-email@example.com>
2021-06-23 20:11:45 +0300

    styles

---
+++ hotel/src/App.js
@@ -0,0 +1,12 @@
+import {Home} from './pages/Home'
+import GlobalStyles from './components/globals/GlobalStyles'
+function App() {
+  return (
+    <>
+    <GlobalStyles />
+    <Home />
+    </>
+  );
+}
+
+export default App;
